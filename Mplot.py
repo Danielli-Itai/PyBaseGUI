67ed749e681ebbe52c804dfe56cc1a83c8234e4b
Itai Danielli <Danielli.Itai@gmail.com>
2020-01-14 14:20:15 +0200

    Add confusion matrix plotter

--- Mplot.py
+++ Mplot.py
@@ -1,10 +1,9 @@
 #Matplot library simple interface functions.
 #Check out another optional library:https://seaborn.pydata.org/index.html
-
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 
 #Create a new plot.
@@ -98,4 +97,55 @@ def Mp2dHistogram(mean, sigma, block:bool=False):
 	plt.show(block=block)
 	return
 
+
+
+
+
+
+
+
+"""
+This function prints and plots the confusion matrix.
+Normalization can be applied by setting `normalize=True`.
+"""
+import numpy as np  # numpy
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+def plot_confusion_matrix(actual_cls:list, predict_cls:list, classes:list, normalize=False, title=None, cmap=plt.cm.Blues, block:bool=True):
+	if not title:
+		if normalize:
+			title = 'Normalized confusion matrix'
+		else:
+			title = 'Confusion matrix, without normalization'
+
+	# Compute confusion matrix
+	conf_mat = confusion_matrix(actual_cls, predict_cls)
+
+	# Only use the labels that appear in the data
+	print("data classes"  +unique_labels(actual_cls, predict_cls))
+
+	if normalize:
+		conf_mat = conf_mat.astype('float') / conf_mat.sum(axis=1)[:, np.newaxis]
+
+	fig, axes = plt.subplots()
+	axes_img = axes.imshow(conf_mat, interpolation='nearest', cmap=cmap)
+	axes.figure.colorbar(axes_img, ax=axes)
+
+	# We want to show all ticks... and label them with the respective list entries
+	axes.set(xticks=np.arange(conf_mat.shape[1]), yticks=np.arange(conf_mat.shape[0]), xticklabels=classes, yticklabels=classes,
+				title=title, ylabel='True label',  xlabel='Predicted label')
+
+	# Rotate the tick labels and set their alignment.
+	plt.setp(axes.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+	# Loop over data dimensions and create text annotations.
+	fmt = '.2f' if normalize else 'd'
+	thresh = conf_mat.max() / 2.
+	for i in range(conf_mat.shape[0]):
+		for j in range(conf_mat.shape[1]):
+			axes.text(j, i, format(conf_mat[i, j], fmt), ha="center", va="center", color="white" if conf_mat[i, j] > thresh else "black")
+
+	fig.tight_layout()
+	plt.show(block=block)
+	return axes
 
